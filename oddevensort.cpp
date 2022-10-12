a6b306adcd9f3947b2cdbcda5d711c0c6c41a90f
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2022-10-11 23:45:50 -0300

    Adding oddeven sort algorithm in cpp

---
+++ oddevensort.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+void oddEvenSort(int arr[], int n) 
+{ 
+    bool isSorted = false;
+    while (!isSorted) 
+    { 
+        isSorted = true; 
+        for (int i=1; i<=n-2; i=i+2) 
+        { 
+            if (arr[i] > arr[i+1]) 
+             { 
+                swap(arr[i], arr[i+1]); 
+                isSorted = false; 
+              } 
+        } 
+        for (int i=0; i<=n-2; i=i+2) 
+        { 
+            if (arr[i] > arr[i+1]) 
+            { 
+                swap(arr[i], arr[i+1]); 
+                isSorted = false; 
+            } 
+        } 
+    } 
+    return; 
+} 
+
+void printArray(int arr[], int n) 
+{ 
+   for (int i=0; i < n; i++) 
+       cout << arr[i] << " "; 
+   cout << "\n"; 
+} 
+
+
+int main() 
+{ 
+    int arr[] = {9,7,3,8,5,6,4,1}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    oddEvenSort(arr, n); 
+    printArray(arr, n); 
+    return (0); 
+}
