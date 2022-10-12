2e07bfdc75aa930a36865af7e560aa79741d8708
nv8867662 <115584661+nv8867662@users.noreply.github.com>
2022-10-12 16:09:27 +0530

    Create bubble-sort.cpp

---
+++ bubble-sort.cpp
@@ -0,0 +1,28 @@
+//recursive implementation
+#include <iostream>
+using namespace std;
+void bubblesort(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    bubblesort(arr, n - 1);
+}
+int main()
+{
+    int arr[5] = {4, 6, 9, 10, 2};
+    bubblesort(arr, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
