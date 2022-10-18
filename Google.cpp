b39f58eb67fa9026615d4421ceec8ee0ac710b75
Illuminati2022 <116081458+Illuminati2022@users.noreply.github.com>
2022-10-18 17:47:35 +0530

    Create Google.cpp

---
+++ Google.cpp
@@ -0,0 +1,40 @@
+#include<iostream.h>
+#include<conio.h>
+#include<graphic.h>
+#include<math.h>
+void main()
+{clrscr();
+int g=0,a;
+initgraph(&g,&d,"");
+setbkcolor(14);
+setcolor(6);
+settextstyle(2,0,4);
+outtextxy(180,130,"G");
+setcolor(5);
+settextstyle(2,0,4);
+outtextxy(120,120,"O");
+setcolor(6);
+settextstyle(2,0,4);
+outtextxy(300,120,"O");
+setcolor(5);
+settextstyle(2,0,4);
+outtextxy(250,130,"G");
+setcolor(2);
+settextstyle(2,0,4);
+outtextxy(360,160,"L");
+setcolor(3);
+settextstyle(2,0,4);
+outtextxy(310,130,"E");
+setcolor(9);
+settextstyle(2,0,4);
+setcolor(8);
+settextstyle(2,0,4);
+outtextxy(110,250,"surf");
+settextstyle(2,0,4);
+outtextxy(350,320,"Go AHEAD");
+setcolor(6);
+rectangle(130,210,450,210);
+rectangle(90,310,170,340);
+rectangle(360,320,510,320);
+getch();
+}
